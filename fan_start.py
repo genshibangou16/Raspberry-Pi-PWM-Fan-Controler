60c6f98a142c46012cbb62b70dada86715f3e1d3
genshibangou16 <64895726+genshibangou16@users.noreply.github.com>
2020-09-30 01:16:38 +0900

    Update fan_start.py

--- fan_start.py
+++ fan_start.py
@@ -1,65 +1,52 @@
 #!/usr/bin/python3.7
 
+#!/usr/bin/python3.7
+
 import pigpio
 import time
-import subprocess
-import os
-import sys
 import datetime
 
-gpio_pin = 18
+pin = 18
 pi = pigpio.pi()
 
 top = 1000000
-bottomL = 220000
-freqL = 150000
-bottomH = 500000
-freqH = 8333000
-high = 70
+bottom = 200000
+hz_day = 60
+hz_night = 160000
+high = 80
 low = 40
+x_range = high - low
 
-def get_temp():
-    return float(subprocess.run('cat /sys/class/thermal/thermal_zone0/temp', shell=True, encoding='utf-8', stdout=subprocess.PIPE).stdout) / 1000
-
-def get_duty(temp = low):
+def get_duty(temp):
     now = datetime.datetime.now().hour
     if now >= 8 and now < 23:
-        hz = freqH
-        btm = bottomH
+        hz = hz_day
     else:
-        hz = freqL
-        btm = bottomL
-    if high < temp:
-        rs = top
-    elif temp < low:
-        rs = btm
+        hz = hz_night
+    if high <= temp:
+        duty = top
+    elif temp <= low:
+        duty = bottom
     else:
-        rs = int(btm + ((top - btm) / (high -low)) * (temp - low))
-    return hz, rs
+        x = temp - low
+        duty = int(bottom + (top - bottom) * x * x / x_range / x_range)
+    return hz, duty
 
-def pwm():
-    pi.set_mode(gpio_pin, pigpio.OUTPUT)
-    pi.write(gpio_pin, 1)
-    while True:
-        count = 0
-        temp = 0
-        while count < 30:
-            temp += get_temp()
-            count += 1
-            time.sleep(1)
-        temp /= 30
-        duty = get_duty(temp)
-        pi.hardware_PWM(gpio_pin, duty[0], duty[1])
+pi.set_mode(pin, pigpio.OUTPUT)
+pi.write(pin, 1)
+time.sleep(30)
 
 def fork():
-    pid = os.fork()
-    if pid > 0:
-        f = open('/run/fan.pid','w')
-        f.write(str(pid)+"\n")
-        f.close()
-        sys.exit()
-    if pid == 0:
-        pwm()
+    while True:
+        i = 0
+        temp = 0
+        for i in range(5):
+            with open('/sys/class/thermal/thermal_zone0/temp') as f:
+                temp += int(f.read())
+            time.sleep(1)
+        temp /= 5000
+        rs = get_duty(temp)
+        pi.hardware_PWM(pin, rs[0], rs[1])
 
 if __name__ == '__main__':
     fork()
